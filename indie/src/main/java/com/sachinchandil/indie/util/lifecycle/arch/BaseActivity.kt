18a62759dac10854f9373088aa705c75c1ca51dc
Sachin Chandil <chandilsachin@gmail.com>
2017-09-02 20:57:23 +0530

    Initial commit

---
+++ indie/src/main/java/com/sachinchandil/indie/util/lifecycle/arch/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.ace.diettracker.util.lifecycle.arch
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import com.ace.diettracker.util.SimpleProgressDialog
+
+/**
+ * Created by sachin on 27/6/17.
+ */
+open class BaseActivity : AppCompatActivity(), SimpleProgressDialog {
+    override var progressBar: ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
